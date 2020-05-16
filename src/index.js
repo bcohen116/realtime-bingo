f80b2260b961ee9a05235b82bab1da1299e44a05
Ben Cohen <benfish116@yahoo.com>
2020-05-15 20:18:26 -0400

    Added project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+const config = {
+  apiKey: auth.env.REACT_APP_API_KEY,
+  authDomain: auth.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: auth.env.REACT_APP_DATABASE_URL,
+  projectId: auth.env.REACT_APP_PROJECT_ID,
+  storageBucket: auth.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: auth.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
